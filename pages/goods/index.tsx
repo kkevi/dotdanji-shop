cf5cd8e66c64c9dc6fe2cad82c347cde5967013c
simbaat-korea <dev@simbaat.com>
2022-03-15 17:17:06 +0900

    컴포넌트 대소문자 변경3

--- pages/goods/index.tsx
+++ pages/goods/index.tsx
@@ -1,11 +1,10 @@
 import MainLayout from "Components2/MainLayout"
-import Home from "Home"
 
 export default function Index() {
     return (
         <div>
             <MainLayout>
-                <Home />
+                <></>
             </MainLayout>
         </div>
     )
