8ca174aa7d705eae428877a2efd3caf425a43bda
kkevi <borakim@simbaat.com>
2022-07-01 16:41:39 +0900

    next config js roll back

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // reactStrictMode: true,
-    distDir: ".next",
-    publicRuntimeConfig: {
-        var: "next.config.js test",
-    },
-    webpackDevMiddleware: config => {
-        config.watchOptions = {
-            poll: 800,
-            aggregateTimeout: 300,
-        }
-        return config
-    },
+    reactStrictMode: true,
+    // distDir: ".next",
+    // publicRuntimeConfig: {
+    //     var: "next.config.js test",
+    // },
+    // webpackDevMiddleware: config => {
+    //     config.watchOptions = {
+    //         poll: 800,
+    //         aggregateTimeout: 300,
+    //     }
+    //     return config
+    // },
     // api_domain: "",
 }
 
