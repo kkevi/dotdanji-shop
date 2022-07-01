b120854d1df0f3e0d6b956055ca530d435d3c1fe
kkevi <borakim@simbaat.com>
2022-07-01 15:44:32 +0900

    next config js test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    // reactStrictMode: true,
+    distDir: ".next",
+    publicRuntimeConfig: {
+        var: "next.config.js test",
+    },
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 800,
+            aggregateTimeout: 300,
+        }
+        return config
+    },
+    // api_domain: "",
 }
 
 module.exports = nextConfig
