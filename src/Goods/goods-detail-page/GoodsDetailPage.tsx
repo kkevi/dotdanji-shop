aea1ff63e2b248193927ef9a5a9ba6aac9c4698e
kkevi <borakim@simbaat.com>
2022-07-20 10:43:01 +0900

    주석 해제 및 패키지제이슨 수정

--- src/Goods/goods-detail-page/GoodsDetailPage.tsx
+++ src/Goods/goods-detail-page/GoodsDetailPage.tsx
@@ -37,42 +37,42 @@ export default function GoodsDetailPage(props: Props) {
     const [selectValueList, setSelectValueList] = useState<OptionCart[]>([])
     const [totalPrice, setTotalPrice] = useState(0)
 
-    // useEffect(() => {
-    //     //전체 금액 표시
-    //     setTotalPrice(
-    //         selectValueList.reduce((acc, cur) => {
-    //             if (!options) return acc
-    //             const optionValue = options.filter(it => it.optionId === cur.optionId)[0].addPlace
-    //             acc += (optionValue + resultPrice) * cur.count
-    //             return acc
-    //         }, 0),
-    //     )
-    // }, [selectValueList, selectValue])
+    useEffect(() => {
+        //전체 금액 표시
+        setTotalPrice(
+            selectValueList.reduce((acc, cur) => {
+                if (!options) return acc
+                const optionValue = options.filter(it => it.optionId === cur.optionId)[0].addPlace
+                acc += (optionValue + resultPrice) * cur.count
+                return acc
+            }, 0),
+        )
+    }, [selectValueList, selectValue])
 
     // 옵션 선택
     const onSelectOption = (event: SelectChangeEvent) => {
         const {value} = event.target
-        // onAddOptionList(value)
-        // setSelectValue(value)
+        onAddOptionList(value)
+        setSelectValue(value)
     }
 
     //선택 옵션 배열 추가
-    // const onAddOptionList = (id: string) => {
-    //     if (id === defaultOption) return
-    //     const search = selectValueList.findIndex(it => it.optionId === id)
-    //     if (search === -1) {
-    //         // 옵션 선택하여 추가
-    //         const arr = options
-    //         const selectOptionId = arr.filter(it => it.optionId === id)[0].optionId
-    //         selectValueList.push({
-    //             optionId: selectOptionId,
-    //             count: 1,
-    //         })
-    //     } else {
-    //         // 이미 추가되어있는 옵션 추가 선택
-    //         alert("이미 선택 되어있는 옵션입니다. 수량변경을 통해 개수를 조절 해주세요.")
-    //     }
-    // }
+    const onAddOptionList = (id: string) => {
+        if (id === defaultOption) return
+        const search = selectValueList.findIndex(it => it.optionId === id)
+        if (search === -1) {
+            // 옵션 선택하여 추가
+            const arr = options
+            const selectOptionId = arr.filter(it => it.optionId === id)[0].optionId
+            selectValueList.push({
+                optionId: selectOptionId,
+                count: 1,
+            })
+        } else {
+            // 이미 추가되어있는 옵션 추가 선택
+            alert("이미 선택 되어있는 옵션입니다. 수량변경을 통해 개수를 조절 해주세요.")
+        }
+    }
 
     // 옵션 배열 삭제
     const onDeleteOption = (optionId: string) => {
@@ -88,50 +88,50 @@ export default function GoodsDetailPage(props: Props) {
         // }
         if (selectValueList.length < 1) return alert("옵션을 선택 해주세요.")
 
-        // try {
-        //     //TODO: 서버 장바구니에 저장 기능 추가
-        //     const data = selectValueList.reduce((acc: CartOptionsType[], cur: OptionCart, index) => {
-        //         const goodsOptionData = options.filter(it => it.optionId === cur.optionId)[0]
-        //         acc.push({
-        //             goodsId: goodsId,
-        //             count: cur.count,
-        //             price: (goodsOptionData.addPlace + resultPrice) * cur.count,
-        //             optionId: cur.optionId,
-        //             optionName: goodsOptionData.name,
-        //             optionAddPlace: goodsOptionData.addPlace,
-        //         })
-        //         return acc
-        //     }, [])
+        try {
+            //TODO: 서버 장바구니에 저장 기능 추가
+            const data = selectValueList.reduce((acc: CartOptionsType[], cur: OptionCart, index) => {
+                const goodsOptionData = options.filter(it => it.optionId === cur.optionId)[0]
+                acc.push({
+                    goodsId: goodsId,
+                    count: cur.count,
+                    price: (goodsOptionData.addPlace + resultPrice) * cur.count,
+                    optionId: cur.optionId,
+                    optionName: goodsOptionData.name,
+                    optionAddPlace: goodsOptionData.addPlace,
+                })
+                return acc
+            }, [])
 
-        //     if (!cartData) {
-        //         // 기존 장바구니가 비어있을 경우
-        //         await setCartData(JSON.stringify(data))
-        //         if (confirm("장바구니를 확인하시겠습니까?")) await Router.push("/cart")
-        //     } else {
-        //         // 장바구니에 하나라도 값이 있을 경우
-        //         const parseCartData = JSON.parse(cartData)
-        //         const data_OptionIds = data.map(itm => {
-        //             return itm.optionId
-        //         })
-        //         const cartData_OptionIds = parseCartData.map((itm: {optionId: any}) => {
-        //             return itm.optionId
-        //         })
-        //         const comparing = data_OptionIds.filter(it => cartData_OptionIds.includes(it))
-        //         if (comparing.length === 0) {
-        //             // 없을 경우
-        //             const newCartData = [...data, ...parseCartData]
-        //             await setCartData(JSON.stringify(newCartData))
-        //             if (confirm("장바구니를 확인하시겠습니까?")) await Router.push("/cart")
-        //         } else {
-        //             // 동일한 옵션이 있을 경우
-        //             if (confirm("장바구니에 동일한 상품이 존재합니다. 장바구니로 이동하시겠습니까?")) {
-        //                 return Router.push("/cart")
-        //             }
-        //         }
-        //     }
-        // } catch (e) {
-        //     console.log("e:", e)
-        // }
+            if (!cartData) {
+                // 기존 장바구니가 비어있을 경우
+                await setCartData(JSON.stringify(data))
+                if (confirm("장바구니를 확인하시겠습니까?")) await Router.push("/cart")
+            } else {
+                // 장바구니에 하나라도 값이 있을 경우
+                const parseCartData = JSON.parse(cartData)
+                const data_OptionIds = data.map(itm => {
+                    return itm.optionId
+                })
+                const cartData_OptionIds = parseCartData.map((itm: {optionId: any}) => {
+                    return itm.optionId
+                })
+                const comparing = data_OptionIds.filter(it => cartData_OptionIds.includes(it))
+                if (comparing.length === 0) {
+                    // 없을 경우
+                    const newCartData = [...data, ...parseCartData]
+                    await setCartData(JSON.stringify(newCartData))
+                    if (confirm("장바구니를 확인하시겠습니까?")) await Router.push("/cart")
+                } else {
+                    // 동일한 옵션이 있을 경우
+                    if (confirm("장바구니에 동일한 상품이 존재합니다. 장바구니로 이동하시겠습니까?")) {
+                        return Router.push("/cart")
+                    }
+                }
+            }
+        } catch (e) {
+            console.log("e:", e)
+        }
     }
 
     //바로 구매하기 클릭
